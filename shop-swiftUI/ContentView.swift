d869807eab4432027d0d3624667b2706fbacca07
devpark435 <devpark435@gmail.com>
2023-06-23 13:16:40 +0900

    add : bottom navigation bar function

--- shop-swiftUI/ContentView.swift
+++ shop-swiftUI/ContentView.swift
@@ -8,15 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    @State var currentTab: Tab = .Home
+    
+    init() {
+        UITabBar.appearance().isHidden = true
     }
+    
+    @Namespace var animation
+    
+    var body: some View {
+        TabView(selection: $currentTab) {
+            Text("Home View")
+            Text("Search View")
+            Text("Notification View")
+            Text("Cart View")
+            Text("Profile View")
+            
+        }
+        .overlay(
+            HStack(spacing:0){
+                ForEach(Tab.allCases,id: \.rawValue){tab in
+                    TabButton(tab: tab)
+                }
+                .padding(.vertical)
+                .padding(.bottom, getSafeArea().bottom == 0 ? 5 : (getSafeArea().bottom - 15))
+                .background(Color("kSecondary"))
+            },alignment: .bottom
+        ).ignoresSafeArea(.all, edges: .bottom)
+       
+    }
+    
+    func TabButton(tab: Tab){
+        GeometryReader{
+            proxy in
+            Button(
+                action: {
+                withAnimation(.spring()){
+                    currentTab = tab
+                }
+            },label: {
+                VStack(spacing:0){
+                    
+                }
+            }
+            )
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -30,5 +68,47 @@ enum Tab: String, CaseIterable{
     case Search = "magnifyingglass.circle"
     case Notification = "bell"
     case Cart = "bag"
-    case Profile = "person"git remote add origin https://github.com/devpark435/Market_SwiftUI.git
+    case Profile = "person"
+    
+    var Tabname: String{
+        switch self{
+        case .Home:
+            return "Home"
+        case .Search:
+            return "Search"
+        case .Notification:
+            return "Notification"
+        case .Cart:
+            return "Cart"
+        case .Profile:
+            return "Profile"
+        }
+    }
+}
+
+extension View{
+    func getSafeArea() -> UIEdgeInsets {
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else{
+            return .zero
+        }
+        guard let safeArea = screen.windows.first?.safeAreaInsets else{
+            return .zero
+        }
+        
+        return safeArea
+    }
+}
+
+struct MaterialEffect: UIViewRepresentable{
+    var style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: style))
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        
+    }
 }
